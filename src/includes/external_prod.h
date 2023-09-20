c36339823e35d22f442e82bad31d64a77dd3bee5
Zhikun Wang <nocrizwang@gmail.com>
2023-09-19 21:28:13 -0500

    uncomment and make compile

--- src/includes/external_prod.h
+++ src/includes/external_prod.h
@@ -3,18 +3,7 @@
 #include <vector>
 
 // A GSWCiphertext is a flattened 2lx2 matrix of polynomials 
-typedef std::vector<std::vector<uint128_t>> GSWCiphertext;
-
-/*!
-  Performs a gadget decomposition of a size 2 BFV ciphertext into 2 sets of rows of l polynomials (the 2 sets are concatenated into a single vector of vectors). Each polynomial coefficient encodes the value congruent to the original ciphertext coefficient modulus the value of base^(l-row).
-  @param ct - input BFV ciphertext. Should be of size 2. 
-  @param l - number of GSW rows
-  @param context_data - SEAL context data
-  @param output - output to store the decomposed ciphertext as a vector of vectors of polynomial coefficients
-  @param base_log2 - value of log2(GSW base) (base must be a power of 2)
-  @param pool - SEAL memory pool
-*/
-void decomp_rlwe128(seal::Ciphertext ct, const uint64_t l, std::shared_ptr<seal::SEALContext::ContextData> context_data, std::vector<std::vector<uint64_t>> &output, int base_log2, seal::util::MemoryPool &pool);
+typedef std::vector<std::vector<uint64_t>> GSWCiphertext;
 
 /*!
   Computes the external product between a GSW ciphertext and a decomposed BFV ciphertext.
@@ -25,12 +14,26 @@ void decomp_rlwe128(seal::Ciphertext ct, const uint64_t l, std::shared_ptr<seal:
   @param ct_poly_size - number of ciphertext polynomials
   @param res_ct - output ciphertext
 */
-void external_product(GSWCiphertext &gsw_enc, std::vector<uint64_t *> &decomposed_bfv, std::shared_ptr<seal::SEALContext> &context, int l, size_t ct_poly_size, seal::Ciphertext &res_ct);
+void external_product(GSWCiphertext &gsw_enc,
+                      std::vector<uint64_t *> &decomposed_bfv,
+                      std::shared_ptr<seal::SEALContext> &context, int l,
+                      size_t ct_poly_size, seal::Ciphertext &res_ct);
 
 /*!
-  Generates a GSW gadget matrix with the specified parameters.
-  @param context_data - SEAL context data
+  Performs a gadget decomposition of a size 2 BFV ciphertext into 2 sets of rows
+  of l polynomials (the 2 sets are concatenated into a single vector of
+  vectors). Each polynomial coefficient encodes the value congruent to the
+  original ciphertext coefficient modulus the value of base^(l-row).
+  @param ct - input BFV ciphertext. Should be of size 2. 
   @param l - number of GSW rows
+  @param context_data - SEAL context data
+  @param output - output to store the decomposed ciphertext as a vector of vectors of polynomial coefficients
   @param base_log2 - value of log2(GSW base) (base must be a power of 2)
+  @param pool - SEAL memory pool
 */
-GSWCiphertext create_GSW_gadget(seal::SEALContext context, int base_log2, int l);
+void decomp_rlwe128(
+    seal::Ciphertext ct, const uint64_t l,
+    std::shared_ptr<seal::SEALContext::ContextData> context_data,
+    std::vector<std::vector<uint64_t>> &output, int base_log2,
+    seal::util::MemoryPool &pool);
+
