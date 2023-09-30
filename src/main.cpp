0daf9d83d76908fee6cb2cfc83fb5a199dd6b093
Zhikun Wang <nocrizwang@gmail.com>
2023-09-30 10:50:32 -0500

    lint

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "pir.h"
 #include "server.h"
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 namespace gsw {
-  uint64_t l;
-  uint64_t base_log2;
-}
+uint64_t l;
+uint64_t base_log2;
+} // namespace gsw
 
 int main() {
   // PirParams pir_params(1048576, 8, 1000000, 3);
@@ -17,15 +17,15 @@ int main() {
   PirServer server(pir_params);
   // server.gen_data();
 
-  #ifdef _DEBUG
-  std::cout<< "===== Debug build =====" <<std::endl;
-  #endif
-  #ifdef _BENCHMARK
-  std::cout<< " ===== Benchmark build =====" <<std::endl;
-  #endif
+#ifdef _DEBUG
+  std::cout << "===== Debug build =====" << std::endl;
+#endif
+#ifdef _BENCHMARK
+  std::cout << " ===== Benchmark build =====" << std::endl;
+#endif
 
   std::vector<Entry> data(pir_params.get_num_entries());
-  for (auto & entry : data) {
+  for (auto &entry : data) {
     entry.push_back(255);
     entry.push_back(173);
     entry.push_back(19);
@@ -41,7 +41,7 @@ int main() {
 
   server.set_client_galois_key(client_id, client.create_galois_keys());
   server.set_client_gsw_key(client_id, client.generate_gsw_from_key());
-  
+
   std::cout << "Client registered" << std::endl;
 
   client.test_external_product();
@@ -50,35 +50,38 @@ int main() {
 
   std::cout << "Result: " << std::endl;
   auto decrypted_result = client.decrypt_result(result);
-  #ifdef _DEBUG
-    for (auto & res : decrypted_result) {
-      //std::cout << res.to_string() << std::endl;
-    }
-  #endif
+#ifdef _DEBUG
+  for (auto &res : decrypted_result) {
+    // std::cout << res.to_string() << std::endl;
+  }
+#endif
   print_entry(client.get_entry_from_plaintext(id, decrypted_result[0]));
 
-  #ifdef _BENCHMARK
-    std::cout << "Noise budget remaining: " << client.get_decryptor()->invariant_noise_budget(result[0]) << " bits" << std::endl;
+#ifdef _BENCHMARK
+  std::cout << "Noise budget remaining: "
+            << client.get_decryptor()->invariant_noise_budget(result[0])
+            << " bits" << std::endl;
 
-    auto query = client.generate_query(5);
-    auto start_time = std::chrono::high_resolution_clock::now();
-    server.make_query_regular_mod(client_id, query);
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout<< "No delayed mod: " << elapsed_time.count() << " ms" <<std::endl;
+  auto query = client.generate_query(5);
+  auto start_time = std::chrono::high_resolution_clock::now();
+  server.make_query_regular_mod(client_id, query);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      end_time - start_time);
+  std::cout << "No delayed mod: " << elapsed_time.count() << " ms" << std::endl;
 
-    start_time = std::chrono::high_resolution_clock::now();
-    server.make_query_delayed_mod(client_id, query);
-    end_time = std::chrono::high_resolution_clock::now();
-    elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout<< "Delayed mod: " << elapsed_time.count() << " ms" <<std::endl;
+  start_time = std::chrono::high_resolution_clock::now();
+  server.make_query_delayed_mod(client_id, query);
+  end_time = std::chrono::high_resolution_clock::now();
+  elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      end_time - start_time);
+  std::cout << "Delayed mod: " << elapsed_time.count() << " ms" << std::endl;
 
-    std::cout << std::endl;
-  #endif
+  std::cout << std::endl;
+#endif
   return 0;
 }
 
-
-// Check timing and noise values 
+// Check timing and noise values
 // Move on to GSW
 // ORAM Paper for GSW details
